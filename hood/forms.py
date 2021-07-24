c12d10cf164033e1e35b2d4f78c0e4a95c9d1953
jLuseno161 <joyluseno2@gmail.com>
2021-07-24 21:35:26 +0300

    forms to create user profile & user class

--- hood/forms.py
+++ hood/forms.py
@@ -1,3 +1,4 @@
+from hood.models import Profile
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
@@ -9,3 +10,17 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['contact','profile_pic', 'bio']
+
--- hood/views.py
+++ hood/views.py
@@ -1,7 +1,7 @@
 from hood.models import Post, Profile
 from django.contrib.auth.models import User
-from hood.forms import SignUpForm
-from django.http.response import HttpResponse
+from hood.forms import SignUpForm, UpdateProfileForm, UpdateUserForm
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
